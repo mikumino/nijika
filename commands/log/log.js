f9131956eff4519692e07b00cbe100052b7e8284
mikumino <36429067+mikumino@users.noreply.github.com>
2023-07-08 17:28:20 -0700

    Starting codebase refactor
    - Split the creation of a one time source into its own file

--- commands/log/log.js
+++ commands/log/log.js
@@ -1,4 +1,6 @@
-const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder, TextInputStyle, Events, ModalBuilder, TextInputBuilder, ModalSubmitFields } = require('discord.js');
+const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder, TextInputStyle, Events, ModalBuilder, TextInputBuilder } = require('discord.js');
+
+const oneTimeSource = require('../../modules/oneTimeSource');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -118,51 +120,16 @@ module.exports = {
                     sourceConfirmation.update({ content: `Source "${title}" successfully created!`, components: [] });
                     // TODO: Add source to database (content type, title, description)
                 }
-
             }
 
             // User chooses to create a one time source
             else if (confirmation.values[0] === 'oneTimeSource') {
                 console.log("enter else if.")
-                const oneTimeMenu = new ModalBuilder()
-                    .setCustomId('oneTimeModal')
-                    .setTitle('One Time Source');
-                const oneTimeTitle = new TextInputBuilder()
-                    .setCustomId('oneTimeTitle')
-                    .setLabel('Title')
-                    .setStyle(TextInputStyle.Short);
-                const oneTimeDescription = new TextInputBuilder()
-                    .setCustomId('oneTimeDescription')
-                    .setLabel('Description')
-                    .setStyle(TextInputStyle.Paragraph);
-                const oneTimeTime = new TextInputBuilder()
-                    .setCustomId('oneTimeTime')
-                    .setLabel('Time (in minutes)')
-                    .setStyle(TextInputStyle.Short)
-
-                const firstActionOneTime = new ActionRowBuilder().addComponents(oneTimeTitle);
-                const secondActionOneTime = new ActionRowBuilder().addComponents(oneTimeDescription);
-                const thridActionOneTime = new ActionRowBuilder().addComponents(oneTimeTime);
-
-                oneTimeMenu.addComponents(firstActionOneTime, secondActionOneTime, thridActionOneTime);
-
-                await confirmation.showModal(oneTimeMenu);
-
-                const oneTimeConfirmation = await confirmation.awaitModalSubmit({ filter: collectorFilter, time: 30000 });
-                
-                if (oneTimeConfirmation) {
-                    // Extract fields from modal submission and assign to variables
-                    const title = oneTimeConfirmation.fields.getTextInputValue('oneTimeTitle');
-                    const description = oneTimeConfirmation.fields.getTextInputValue('oneTimeDescription');
-                    const time = oneTimeConfirmation.fields.getTextInputValue('oneTimeTime')
-                    oneTimeConfirmation.update({ content: `Source "${title}" for ${time} minutes was successfully logged!`, components: [] });
-                    // TODO: Add source to database (content type, title, description)
-                }
-                
+                oneTimeSource.execute(confirmation, collectorFilter);
             }
 
             else {
-                // Handle user's sources
+                await interaction.update({ content: 'dont click this one yet', components: [] });
             }
         } catch (e) {
             console.log(e);
