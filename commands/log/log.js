4d99d69f652c922225d84945ca285c18064ae3eb
mikumino <36429067+mikumino@users.noreply.github.com>
2023-07-08 17:43:17 -0700

    Split createSource into module

--- commands/log/log.js
+++ commands/log/log.js
@@ -1,5 +1,6 @@
 const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder, TextInputStyle, Events, ModalBuilder, TextInputBuilder } = require('discord.js');
 
+const createSource = require('../../modules/createSource');
 const oneTimeSource = require('../../modules/oneTimeSource');
 
 module.exports = {
@@ -28,12 +29,12 @@ module.exports = {
 
         const row = new ActionRowBuilder()
             .addComponents(select_source);
-        
+
         const response = await interaction.reply({
             content: 'Select a previous immersion source, or create a new / temporary one.',
             components: [row],
         });
-        
+
         const collectorFilter = i => i.user.id === interaction.user.id;
 
         try {
@@ -42,84 +43,7 @@ module.exports = {
 
             // If user selects createSource, create a new source
             if (confirmation.values[0] === 'createSource') {    // values[0] because only one option can be selected
-                console.log("createSource");
-                // Build menu for selecting content type
-                const select_content_type = new StringSelectMenuBuilder()
-                    .setCustomId('contentType')
-                    .setPlaceholder('Make a selection!')
-                    .addOptions(
-                        new StringSelectMenuOptionBuilder()
-                            .setLabel('Book')
-                            .setValue('book'),
-                        new StringSelectMenuOptionBuilder()
-                            .setLabel('Anime')
-                            .setValue('anime'),
-                        new StringSelectMenuOptionBuilder()
-                            .setLabel('Manga')
-                            .setValue('manga'),
-                        new StringSelectMenuOptionBuilder()
-                            .setLabel('Visual Novel')
-                            .setValue('vn'),
-                        new StringSelectMenuOptionBuilder()
-                            .setLabel('Video Game')
-                            .setValue('game'),
-                        new StringSelectMenuOptionBuilder()
-                            .setLabel('Reading')
-                            .setValue('reading'),
-                        new StringSelectMenuOptionBuilder()
-                            .setLabel('Listening')
-                            .setValue('listening'),
-                        new StringSelectMenuOptionBuilder()
-                            .setLabel('Other')
-                            .setValue('other')
-                    )
-                // Build action row for menu
-                const row2 = new ActionRowBuilder()
-                    .addComponents(select_content_type);
-                // Update message to prompt user to select content type
-                await confirmation.update({
-                    content: 'Select the type of content you are logging.',
-                    components: [row2],
-                });
-
-                const typeConfirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 30000 });
-
-                // Assign content type to variable
-                const contentType = typeConfirmation.values[0];
-                console.log(contentType);
-
-                // Build modal for user to input title and description
-                const sourceMenu = new ModalBuilder()
-                    .setCustomId('sourceModal')
-                    .setTitle('New Source');
-                const sourceTitle = new TextInputBuilder()
-                    .setCustomId('sourceTitle')
-                    .setLabel('Title')
-                    .setStyle(TextInputStyle.Short);
-                const sourceDescription = new TextInputBuilder()
-                    .setCustomId('sourceDescription')
-                    .setLabel('Description')
-                    .setStyle(TextInputStyle.Paragraph);
-                    
-
-                const firstAction = new ActionRowBuilder().addComponents(sourceTitle);
-                const secondAction = new ActionRowBuilder().addComponents(sourceDescription);
-
-                sourceMenu.addComponents(firstAction, secondAction);
-
-                // Prompt user with modal
-                await typeConfirmation.showModal(sourceMenu);
-
-                // Wait for modal submission and assign values to variables
-                const sourceConfirmation = await typeConfirmation.awaitModalSubmit({ filter: collectorFilter, time: 30000 });
-                
-                if (sourceConfirmation) {
-                    // Extract fields from modal submission and assign to variables
-                    const title = sourceConfirmation.fields.getTextInputValue('sourceTitle');
-                    const description = sourceConfirmation.fields.getTextInputValue('sourceDescription');
-                    sourceConfirmation.update({ content: `Source "${title}" successfully created!`, components: [] });
-                    // TODO: Add source to database (content type, title, description)
-                }
+                createSource.execute(confirmation, collectorFilter);
             }
 
             // User chooses to create a one time source
@@ -138,5 +62,5 @@ module.exports = {
 
 
     }
-    
+
 }
--- modules/oneTimeSource.js
+++ modules/oneTimeSource.js
@@ -3,41 +3,39 @@ const { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = req
 module.exports = {
     async execute(confirmation, collectorFilter) {
         // User chooses to create a one time source
-        if (confirmation.values[0] === 'oneTimeSource') {
-            const oneTimeMenu = new ModalBuilder()
-                .setCustomId('oneTimeModal')
-                .setTitle('One Time Source');
-            const oneTimeTitle = new TextInputBuilder()
-                .setCustomId('oneTimeTitle')
-                .setLabel('Title')
-                .setStyle(TextInputStyle.Short);
-            const oneTimeDescription = new TextInputBuilder()
-                .setCustomId('oneTimeDescription')
-                .setLabel('Description')
-                .setStyle(TextInputStyle.Paragraph);
-            const oneTimeTime = new TextInputBuilder()
-                .setCustomId('oneTimeTime')
-                .setLabel('Time (in minutes)')
-                .setStyle(TextInputStyle.Short)
+        const oneTimeMenu = new ModalBuilder()
+            .setCustomId('oneTimeModal')
+            .setTitle('One Time Source');
+        const oneTimeTitle = new TextInputBuilder()
+            .setCustomId('oneTimeTitle')
+            .setLabel('Title')
+            .setStyle(TextInputStyle.Short);
+        const oneTimeDescription = new TextInputBuilder()
+            .setCustomId('oneTimeDescription')
+            .setLabel('Description')
+            .setStyle(TextInputStyle.Paragraph);
+        const oneTimeTime = new TextInputBuilder()
+            .setCustomId('oneTimeTime')
+            .setLabel('Time (in minutes)')
+            .setStyle(TextInputStyle.Short)
 
-            const firstActionOneTime = new ActionRowBuilder().addComponents(oneTimeTitle);
-            const secondActionOneTime = new ActionRowBuilder().addComponents(oneTimeDescription);
-            const thridActionOneTime = new ActionRowBuilder().addComponents(oneTimeTime);
+        const firstActionOneTime = new ActionRowBuilder().addComponents(oneTimeTitle);
+        const secondActionOneTime = new ActionRowBuilder().addComponents(oneTimeDescription);
+        const thridActionOneTime = new ActionRowBuilder().addComponents(oneTimeTime);
 
-            oneTimeMenu.addComponents(firstActionOneTime, secondActionOneTime, thridActionOneTime);
+        oneTimeMenu.addComponents(firstActionOneTime, secondActionOneTime, thridActionOneTime);
 
-            await confirmation.showModal(oneTimeMenu);
+        await confirmation.showModal(oneTimeMenu);
 
-            const oneTimeConfirmation = await confirmation.awaitModalSubmit({ filter: collectorFilter, time: 30000 });
-            
-            if (oneTimeConfirmation) {
-                // Extract fields from modal submission and assign to variables
-                const title = oneTimeConfirmation.fields.getTextInputValue('oneTimeTitle');
-                const description = oneTimeConfirmation.fields.getTextInputValue('oneTimeDescription');
-                const time = oneTimeConfirmation.fields.getTextInputValue('oneTimeTime')
-                oneTimeConfirmation.update({ content: `Source "${title}" for ${time} minutes was successfully logged!`, components: [] });
-                // TODO: Add source to database (content type, title, description)
-            }
+        const oneTimeConfirmation = await confirmation.awaitModalSubmit({ filter: collectorFilter, time: 30000 });
+
+        if (oneTimeConfirmation) {
+            // Extract fields from modal submission and assign to variables
+            const title = oneTimeConfirmation.fields.getTextInputValue('oneTimeTitle');
+            const description = oneTimeConfirmation.fields.getTextInputValue('oneTimeDescription');
+            const time = oneTimeConfirmation.fields.getTextInputValue('oneTimeTime')
+            oneTimeConfirmation.update({ content: `Source "${title}" for ${time} minutes was successfully logged!`, components: [] });
+            // TODO: Add source to database (content type, title, description)
         }
 
     }
