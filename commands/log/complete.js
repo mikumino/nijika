8dc5e390087e089d180f40e3df5723e064f763b3
Colby Deason <colbydeason@gmail.com>
2023-07-10 19:03:33 -0500

    Complete works, but if you have no sources it errors.

--- commands/log/complete.js
+++ commands/log/complete.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, SlashCommandBuilder, StringSelectMenuBuilder } = require('discord.js')
+const { StringSelectMenuOptionBuilder, ActionRowBuilder, SlashCommandBuilder, StringSelectMenuBuilder, EmbedBuilder } = require('discord.js')
 
 const Source = require('../../models/Source');
 
@@ -13,7 +13,7 @@ module.exports = {
             .setThumbnail('https://cdn.discordapp.com/avatars/1125900859528712212/4ff91215c19043f95527d55c5b9cc491.webp?size=512&width=0&height=0')
             .setColor('#ffe17e')
         
-        const sources = await Source.findAll({ where: { userId: interaction.userId, completed: false, oneTime: false}});
+        const sources = await Source.findAll({ where: { userId: interaction.user.id, completed: false, oneTime: false}});
 
         const select_source = new StringSelectMenuBuilder()
             .setCustomId('Title')
@@ -22,8 +22,7 @@ module.exports = {
             select_source.addOptions(
                 new StringSelectMenuOptionBuilder()
                     .setLabel(source.sourceName)
-                    .setDescription(source.sourceType)
-                    .setValue(source.sourceName)
+                    .setValue(source.sourceId.toString())
             );
         });
 
@@ -37,7 +36,27 @@ module.exports = {
 
         const collectorFilter = i => i.user.id === interaction.user.id;
 
-    
+        try {
+            const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 15000 });
+
+            const completedSource = await Source.findByPk(confirmation.values[0]);
+            completedSource.completed = true;
+            await completedSource.save()
+
+            const embed2 = new EmbedBuilder()
+                .setTitle('Source Completed')
+                .setDescription(`Source "${completedSource.sourceName}" marked as complete! Congrats!`)
+                .setThumbnail('https://cdn.discordapp.com/avatars/1125900859528712212/4ff91215c19043f95527d55c5b9cc491.webp?size=512&width=0&height=0')
+                .setColor('#ffe17e')
+
+            confirmation.update({ embeds: [embed2], components: []});
+            
+        } catch (e) {
+            console.log(e);
+            await interaction.editReply({content: 'Do you even really immerse??? :( I\'m impatient', components: []});
+        }
+
+        
 
     }
 }
