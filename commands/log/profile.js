fc2bee3920aadf8c42b8e57674d73a9397dc235e
mikumino <36429067+mikumino@users.noreply.github.com>
2023-07-12 18:43:35 -0700

    /profile basic functionality
    - users can view their xp
    - potential future changes
     - last logged? favorite content type?

--- commands/log/profile.js
+++ commands/log/profile.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const User = require('../../models/User');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,5 +8,18 @@ module.exports = {
         .setDescription('View your profile.'),
     async execute(interaction) {
         // Get User object from database
+        const [user, created] = await User.findOrCreate({ where: {userId: interaction.user.id} });
+
+        // Build embed
+        const embed = new EmbedBuilder()
+            .setTitle(`${interaction.user.username}`)
+            .setFields(
+                { name: 'XP', value: `${user.XP}`, inline: true },
+            )
+            .setThumbnail(interaction.user.avatarURL())
+            .setColor('#ffe17e');
+
+        // Send it lol
+        await interaction.reply({ embeds: [embed] });
     }
 }
