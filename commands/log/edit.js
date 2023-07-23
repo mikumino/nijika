5564188513110253cc41b2b4e83a9cf4c20af6e7
mikumino <36429067+mikumino@users.noreply.github.com>
2023-07-23 12:18:22 -0700

    Update source info command

--- commands/log/edit.js
+++ commands/log/edit.js
@@ -1,25 +1,30 @@
 const { SlashCommandBuilder, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder } = require('discord.js');
 
+const updateInfo = require('../../modules/updateSource');
 const pauseSource = require('../../modules/pauseSource');
 const resumeSource = require('../../modules/resumeSource');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('pause')
-        .setDescription('Pause or resume a Source'),
+        .setName('edit')
+        .setDescription('Edit a source\'s info, pause, or resume it.'),
     async execute(interaction) {
         // Build embed for initial prompt
         const embed = new EmbedBuilder()
-            .setTitle('Pause or resume a Source')
-            .setDescription('Select whether you would like to pause or resume a source.')
+            .setTitle('Edit a source')
+            .setDescription('Select how you want to edit a source.')
             .setThumbnail(interaction.user.avatarURL())
             .setColor('#ffe17e')
         
         // Build select menu
         const select_pause = new StringSelectMenuBuilder()
-            .setCustomId('pause_menu')
+            .setCustomId('edit_menu')
             .setPlaceholder('Make a selection!')
             .addOptions(
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Update Info')
+                    .setDescription('Update an in progresss source\'s info (title/description).')
+                    .setValue('update'),
                 new StringSelectMenuOptionBuilder()
                     .setLabel('Pause')
                     .setDescription('Pause a source.')
@@ -42,6 +47,11 @@ module.exports = {
         try {
             // Wait for user to select an option
             const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 15000 });
+
+            // If user selects update info, update info
+            if (confirmation.values[0] === 'update') {
+                updateInfo.execute(confirmation, collectorFilter);
+            }
 
             // If user selects pause, pause a source
             if (confirmation.values[0] === 'pause') {
---
+++ modules/updateSource.js
@@ -0,0 +1,112 @@
+const { EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+
+const Source = require('../models/Source');
+
+module.exports = {
+    async execute(confirmation, collectorFilter) {
+        const sources = await Source.findAll({ where: { status: 'In Progress', userId: confirmation.user.id } });
+
+        if (sources.length === 0) {
+            confirmation.update({ content: 'You have no sources to resume.', components: [], embeds: [] });
+            return;
+        }
+
+        const embed = new EmbedBuilder()
+            .setTitle('Update Source')
+            .setDescription('Select the source you would like to update.')
+            .setThumbnail(confirmation.user.avatarURL())
+            .setColor('#ffe17e');
+
+        const select_source = new StringSelectMenuBuilder()
+            .setCustomId('source_update')
+            .setPlaceholder('Make a selection!');
+
+        sources.forEach(source => {
+            select_source.addOptions(
+                new StringSelectMenuOptionBuilder()
+                    .setLabel(source.sourceName)
+                    .setDescription(source.sourceType)
+                    .setValue(source.sourceId.toString())
+            );
+        });
+
+        const row = new ActionRowBuilder().addComponents(select_source);
+        await confirmation.update({ embeds: [embed], components: [row] });
+
+        try {
+            const sourceConfirmation = await confirmation.message.awaitMessageComponent({ filter: collectorFilter, time: 30000 });
+
+            const source = await Source.findByPk(sourceConfirmation.values[0]);
+
+            const updateModal = new ModalBuilder()
+                .setCustomId('updateModal')
+                .setTitle('Update Source');
+            const sourceTitle = new TextInputBuilder()
+                .setCustomId('sourceTitle')
+                .setLabel('Title (Leave blank to keep current)')
+                .setStyle(TextInputStyle.Short)
+                .setRequired(false)
+            const sourceDescription = new TextInputBuilder()
+                .setCustomId('sourceDescription')
+                .setLabel('Description (Leave blank to keep current)')
+                .setStyle(TextInputStyle.Paragraph)
+                .setRequired(false)
+
+            const row1 = new ActionRowBuilder().addComponents(sourceTitle);
+            const row2 = new ActionRowBuilder().addComponents(sourceDescription);
+
+            updateModal.addComponents(row1, row2);
+
+            await sourceConfirmation.showModal(updateModal);
+
+            const updateConfirmation = await sourceConfirmation.awaitModalSubmit({ filter: collectorFilter, time: 30000 });
+
+            if (updateConfirmation.message.id !== confirmation.message.id) {
+                throw new Error('Wrong message ID');
+            }
+
+            if (updateConfirmation) {
+                const title = updateConfirmation.fields.getTextInputValue('sourceTitle');
+                const description = updateConfirmation.fields.getTextInputValue('sourceDescription');
+
+                // Check exists
+                const existingSource = await Source.findOne({ where: { sourceName: title, userId: confirmation.user.id } });
+                if (existingSource) {
+                    embed
+                        .setTitle('Source Already Exists')
+                        .setDescription(`You already have a source named "${title}". Please try again.`);
+                    updateConfirmation.update({ embeds: [embed], components: [] });
+                    return;
+                }
+
+                if (title !== '') {
+                    source.sourceName = title;
+                }
+                if (description !== '') {
+                    source.sourceDescription = description;
+                }
+
+                await source.save();
+
+                let sourceEmbedDesc = source.sourceDescription;
+                if (sourceEmbedDesc === '') {
+                    sourceEmbedDesc = 'No description.';
+                }
+
+                embed
+                    .setTitle('Source Updated')
+                    .setDescription(`${source.sourceType} Source "${source.sourceName}" updated.`)
+                    .setFields(
+                        { name: 'Title', value: source.sourceName, inline: true },
+                        { name: 'Description', value: sourceEmbedDesc, inline: true }
+                    )
+                
+                await updateConfirmation.update({ embeds: [embed], components: [] });
+            }
+            
+        } catch (e) {
+            console.log(e);
+            await confirmation.editReply({ content: 'Interaction expired. Please try again.', embeds: [], components: [] });
+        }
+    }
+}
