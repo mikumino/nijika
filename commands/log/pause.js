7b13a2cb6bac8df165a25c26a8b2365f3b9bdae2
mikumino <36429067+mikumino@users.noreply.github.com>
2023-07-23 11:34:55 -0700

    Source pausing

--- commands/log/pause.js
+++ commands/log/pause.js
@@ -1,4 +1,7 @@
-const { SlashCommandBuilder, EmbedBuilder, StringSelectMenuBuilder, ActionRowBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder } = require('discord.js');
+
+const pauseSource = require('../../modules/pauseSource');
+const resumeSource = require('../../modules/resumeSource');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -17,8 +20,14 @@ module.exports = {
             .setCustomId('pause_menu')
             .setPlaceholder('Make a selection!')
             .addOptions(
-                { label: 'Pause', description: 'Pause a source', value: 'pause' },
-                { label: 'Resume', description: 'Resume a source', value: 'resume' }
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Pause')
+                    .setDescription('Pause a source.')
+                    .setValue('pause'),
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Resume')
+                    .setDescription('Resume a source.')
+                    .setValue('resume')
             );
 
         // Build action row
@@ -26,5 +35,26 @@ module.exports = {
 
         // Send initial prompt
         const response = await interaction.reply({ embeds: [embed], components: [row] });
+
+        // Filter for collector
+        const collectorFilter = i => i.user.id === interaction.user.id;
+
+        try {
+            // Wait for user to select an option
+            const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 15000 });
+
+            // If user selects pause, pause a source
+            if (confirmation.values[0] === 'pause') {
+                pauseSource.execute(confirmation, collectorFilter);
+            }
+
+            // User chooses to resume a source
+            else if (confirmation.values[0] === 'resume') {
+                resumeSource.execute(confirmation, collectorFilter);
+            }
+        } catch (e) {
+            console.log(e);
+            await interaction.editReply({ content: 'Interaction expired. Please try again.', embeds: [], components: [] });
+        }
     }
 }
---
+++ modules/pauseSource.js
@@ -0,0 +1,56 @@
+const { EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder } = require('discord.js');
+
+const Source = require('../models/Source');
+
+module.exports = {
+    async execute(confirmation, collectorFilter) {
+        // Get all In Progress sources
+        const sources = await Source.findAll({ where: { status: 'In Progress', userId: confirmation.user.id } });
+
+        // If user has no In Progress sources, send message and return
+        if (sources.length === 0) {
+            confirmation.editReply({ content: 'You have no sources to pause.', components: [], embeds: [] });
+            return;
+        }
+
+        const embed = new EmbedBuilder()
+            .setTitle('Pause Source')
+            .setDescription('Select the source you would like to pause.')
+            .setThumbnail(confirmation.user.avatarURL())
+            .setColor('#ffe17e');
+
+        const select_source = new StringSelectMenuBuilder()
+            .setCustomId('source_pause')
+            .setPlaceholder('Make a selection!');
+        
+        sources.forEach(source => {
+            select_source.addOptions(
+                new StringSelectMenuOptionBuilder()
+                    .setLabel(source.sourceName)
+                    .setDescription(source.sourceType)
+                    .setValue(source.sourceId.toString())
+            );
+        });
+
+        const row = new ActionRowBuilder().addComponents(select_source);
+        await confirmation.update({ embeds: [embed], components: [row] });
+
+        try {
+            const sourceConfirmation = await confirmation.message.awaitMessageComponent({ filter: collectorFilter, time: 30000 });
+
+            const source = await Source.findByPk(sourceConfirmation.values[0]);
+            source.status = 'Paused';
+            await source.save();
+
+            embed
+                .setTitle('Source Paused')
+                .setDescription(`${source.sourceType} Source "${source.sourceName}" paused.`)
+
+            await sourceConfirmation.update({ embeds: [embed], components: [] });
+
+        } catch (e) {
+            console.log(e);
+            await confirmation.editReply({ content: 'Interaction expired. Please try again.', embeds: [], components: [] });
+        }
+    }
+}
