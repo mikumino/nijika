33001d7ee98ddff5eb23c80a0618665e11b97961
mikumino <36429067+mikumino@users.noreply.github.com>
2023-08-06 18:26:26 -0700

    Toggle daily leaderboard command
    - opt in or out of daily leaderboards with /toggledaily

--- commands/log/toggledaily.js
+++ commands/log/toggledaily.js
@@ -1,10 +1,29 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const config = path.join(__dirname, '../../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('toggledaily')
         .setDescription('Toggles daily leaderboard.'),
     async execute(interaction) {
-        
+        try {
+            // Check perms
+            if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+                return await interaction.reply({ content: 'You do not have permission to use this command.' });
+            }
+
+            const configData = JSON.parse(fs.readFileSync(config));
+
+            // Toggle daily
+            configData.dailyLeaderboard = !configData.dailyLeaderboard;
+            fs.writeFileSync(config, JSON.stringify(configData, null, 4));
+            await interaction.reply({ content: `Daily leaderboard toggled ${configData.dailyLeaderboard ? 'on' : 'off'}.` });
+        }
+        catch (e) {
+            console.log(e);
+            await interaction.reply({ content: 'Something went wrong. Please try again.' });
+        }
     }
 }
