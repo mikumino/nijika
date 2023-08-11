8c956417a00b4303fae2db2db47692daf4fe57ba
Colby Deason <colbydeason@gmail.com>
2023-08-11 00:01:46 -0500

    Added Autocomplete functionality in qlog, with the handling in interactionCreate

--- commands/log/qlog.js
+++ commands/log/qlog.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const User = require('../../models/User');
 const Source = require('../../models/Source');
 
 module.exports = {
@@ -6,7 +7,29 @@ module.exports = {
         .setName('qlog')
         .setDescription('Quick log pre-existing source')
         .addStringOption(option =>
-            option.setName('query')
-                .setDescription('Phrase to search for')
-                .setAutoComplete(true)),
-}
+            option.setName('source')
+                .setDescription('Source to log')
+                .setAutocomplete(true)
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('time')
+                .setDescription('Time to log')
+                .setRequired(true)),
+    async autocomplete(interaction) {
+        const focusedValue = interaction.options.getFocused();
+        const [user, created] = await User.findOrCreate({ where: { userId: interaction.user.id}});
+        const sources = await Source.findAll({ where: { userId: interaction.user.id, status: "In Progress", oneTime: false}});
+        const choices = [];
+        sources.forEach(source => {
+            console.log(source.sourceName);
+            choices.push(source.sourceName);
+        })
+        const filtered = choices.filter(choice => choice.startsWith(focusedValue));
+        await interaction.respond(
+            filtered.map(choice => ({ name: choice, value: choice}))
+        );
+    },
+    async execute(interaction) {
+        
+    },
+};
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,7 +3,20 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+		if (interaction.isAutocomplete()) {
+			const command = interaction.client.commands.get(interaction.commandName);
+
+			if(!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
+
+			try {
+				await command.autocomplete(interaction);
+			} catch (error) {
+				console.error(error);
+			}
+		} 
 
 		const command = interaction.client.commands.get(interaction.commandName);
 
