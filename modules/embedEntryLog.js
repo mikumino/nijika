d64af71d99c468569c4929eb68bbc1f5e16a550d
mikumino <36429067+mikumino@users.noreply.github.com>
2023-08-15 14:22:35 -0700

    embedEntryLog hoursmins
    Completes #9

--- modules/embedEntryLog.js
+++ modules/embedEntryLog.js
@@ -1,14 +1,25 @@
 const { EmbedBuilder } = require('discord.js');
 const Log = require('../models/Log');
+const { toHoursMins } = require('../modules/utils/datetimeUtils');
 
 module.exports = {
     execute(log, source, interaction) {
         if (source.sourceDescription == "") {
             source.sourceDescription = "No description.";
         }
-        return new EmbedBuilder()
-            .setTitle(`${interaction.user.username} logged ${log.duration} minutes! 🎉`)
-            .setDescription(`Source "${source.sourceName}" has been logged ${log.duration} minutes!`)
+        // Hours mins string
+        const hoursMins = toHoursMins(log.duration);
+        let hoursMinsString = "";
+        if (hoursMins.hours == 0) {
+            hoursMinsString = `${hoursMins.mins} minutes`;
+        } else if (hoursMins.hours == 1) {
+            hoursMinsString = `${hoursMins.hours} hour and ${hoursMins.mins} minutes`;
+        } else {
+            hoursMinsString = `${hoursMins.hours} hours and ${hoursMins.mins} minutes`;
+        }   // im sure theres a better way, but this works for now
+        return new EmbedBuilder() 
+            .setTitle(`${interaction.user.username} logged ${hoursMinsString}! 🎉`)
+            .setDescription(`Source "${source.sourceName}" has been logged ${hoursMinsString}!`)
             .setFields(
                 { name: 'Title', value: `${source.sourceName}`, inline: true },
                 { name: 'Description', value: `${source.sourceDescription}`, inline: true },
