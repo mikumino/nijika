32c210ef883efdf6abfb02d52740a891abc9cc16
mikumino <36429067+mikumino@users.noreply.github.com>
2023-08-20 13:49:14 -0700

    Added presence

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,14 @@
 const { Events } = require('discord.js');
+const { ActivityType } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+        client.user.setActivity({
+            name: 'LIVE @ STARRY',
+            type: ActivityType.Playing,
+        });
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
