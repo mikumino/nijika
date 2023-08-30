db5765ec77ca431f190f18944d2f8dd44ff98e28
mikumino <36429067+mikumino@users.noreply.github.com>
2023-08-30 16:17:51 -0700

    Monthly auto summary

--- modules/dailyLeaderboard.js
+++ modules/dailyLeaderboard.js
@@ -2,10 +2,11 @@ const cron = require('node-cron');
 const createLeaderboard = require('./createLeaderboard');
 const images = require('./images.json');
 
-let cronJob;
+let dailyJob, monthlyJob;
 
 async function start(channel) {
-    cronJob = cron.schedule('0 2 * * *', async () => {
+    // daily job
+    dailyJob = cron.schedule('0 2 * * *', async () => {
         try {
             const randomImage = images.rest[Math.floor(Math.random() * images.rest.length)];
             console.log('Sending daily message.');
@@ -24,10 +25,29 @@ async function start(channel) {
             channel.send("AAAAAAAAAAAAAAAAH");
         }
     });
+    // monthly job
+    monthlyJob = cron.schedule('0 2 1 * *', async () => {
+        try {
+            const randomImage = images.rest[Math.floor(Math.random() * images.rest.length)];
+            console.log('Sending monthly message.');
+            const now = new Date();
+            const startDate = new Date(now.getFullYear(), now.getMonth() - 1, 1);
+            startDate.setHours(0, 0, 0, 0);
+            const endDate = new Date(now.getFullYear(), now.getMonth(), 0); 
+            endDate.setHours(23, 59, 59, 999);
+            const embed = await createLeaderboard.create(channel, startDate, endDate);
+            embed.setTitle(`${channel.guild.name}'s Monthly Summary 📅`);
+            embed.setImage(randomImage);
+            channel.send({ embeds: [embed] });
+        } catch (e) {
+            console.log(e);
+            channel.send("AAAAAAAAAAAAAAAAH");
+        }
+    });
 }
 
 function stop() {
-    cronJob.stop();
+    dailyJob.stop();
 }
 
 async function forceSend(channel) {
