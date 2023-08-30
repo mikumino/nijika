4ccdd21b7f1254a03ed726abf31105bc6ae6f1ae
mikumino <36429067+mikumino@users.noreply.github.com>
2023-08-30 15:19:06 -0700

    Corrected comment

--- modules/utils/datetimeUtils.js
+++ modules/utils/datetimeUtils.js
@@ -1,6 +1,6 @@
 // Util file with datetime-related functions
 
-// Given minutes, return a string with the hours and minutes
+// Given minutes, return an object with the hours and minutes
 exports.toHoursMins = (duration) => {
     let hours = Math.floor(duration / 60);
     let mins = duration % 60;
