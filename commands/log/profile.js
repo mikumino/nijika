9670580cfe4b91d4889bec457689911eea7fc2fd
mikumino <36429067+mikumino@users.noreply.github.com>
2023-10-10 19:18:28 -0700

    Add time to /profile

--- commands/log/profile.js
+++ commands/log/profile.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const datetimeUtils = require('../../modules/utils/datetimeUtils');
 
 const User = require('../../models/User');
 
@@ -10,11 +11,16 @@ module.exports = {
         // Get User object from database
         const [user, created] = await User.findOrCreate({ where: {userId: interaction.user.id} });
 
+        // Convert XP to HHhMMm format
+        const XP = Math.round((user.XP / 1.2) * 10) / 10;
+        const XPString = datetimeUtils.toHoursMinsShortString(XP);
+
         // Build embed
         const embed = new EmbedBuilder()
             .setTitle(`${interaction.user.username}`)
             .setFields(
-                { name: 'XP', value: `${user.XP}`, inline: true },
+                { name: 'XP', value: `${user.XP}`, inline: false },
+                { name: 'Total Time', value: `${XPString}`, inline: false },
             )
             .setThumbnail(interaction.user.avatarURL())
             .setColor('#ffe17e');
